5ad3445dae5d9fd639bef0f67b54e463027bf5f9
Sharlikran <danielhmpdx16@gmail.com>
2020-01-29 00:53:04 -0800

    Update Index and Pagebuilder

---
+++ _pagebuilder/buildall.bat
@@ -0,0 +1,14 @@
+C:\Python38-32\python.exe wtxt-wiki.py genHtml index.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 2-wryebashfeatures.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 3-wryemashusage.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 4-generalusage.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 5-advancedusage.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 6-technicalreferences.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 7-tomeofpsymon.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 8-tomeofalt3rn1ty.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 9-morrowindandbeyond.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 10-contribute.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 11-tutorial_Links.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 12-other_tools_and_extras.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 13-oblivionandbeyond.txt
+REM C:\Python38-32\python.exe wtxt-wiki.py genHtml 14-testoolghostwheel.txt
